1492b429f39286947ac2dbb2fe34493a078e8202
Emmanuel <eadjeifrimpong@worcester.edu>
2019-08-24 14:28:33 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'ZERO':
+      return 0;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counterReducer);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>{store.getState()}</div>
+     <button onClick={e => store.dispatch({type: 'INCREMENT'})}>plus</button>
+     <button onClick={e => store.dispatch({type: 'DECREMENT'})}>minus</button>
+     <button onClick={e => store.dispatch({type: 'ZERO'})}>zero</button>
     </div>
   );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
